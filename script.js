b1370a573b989c85ede8b80928155b3aaadb03c5
Arkin <arkinmodi@gmail.com>
2018-06-15 11:02:58 -0400

    added commenting

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById('submit').onclick = function () {
     var b = $("#bvalue").val();
     var c = $("#cvalue").val();
     
+    // gets a, b, c values
     a = parseFloat(a);
     b = parseFloat(b);
     c = parseFloat(c);
@@ -10,9 +11,13 @@ document.getElementById('submit').onclick = function () {
     var discriminant = (Math.pow(b, 2) - 4*a*c);
     var denominator = (2*a);
     console.log(a)
+    
+    //Chekcs if input is valid
     if (isNaN(a) || isNaN(b) || isNaN(c) || a == 0) {
         document.getElementById("xValueFirst").innerHTML = "Must be a Quadratic Equation";
     }
+    
+    //Checks if roots are real
     else if (discriminant >= 0) {
         var positiveNumerator = (-1*b + Math.sqrt(discriminant));
         var negativeNumerator = (-1*b - Math.sqrt(discriminant));
@@ -22,7 +27,10 @@ document.getElementById('submit').onclick = function () {
 
         document.getElementById("xValueFirst").innerHTML = finalAnswerPlus;
         document.getElementById("xValueSecond").innerHTML = finalAnswerNegative;
-    } else {
+    } 
+    
+    //Checks if roots are imaginary
+    else {
         var real = (-1*b);
         var imaginary = Math.sqrt(Math.abs(discriminant));
         
